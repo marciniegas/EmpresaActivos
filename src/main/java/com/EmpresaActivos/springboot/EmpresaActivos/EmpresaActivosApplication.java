f3d2dfd241ee7a74176706a7dcec288a7d36a312
marciniegas <arc.miguelangel@gmail.com>
2022-09-24 12:10:08 -0500

    first comit

---
+++ src/main/java/com/EmpresaActivos/springboot/EmpresaActivos/EmpresaActivosApplication.java
@@ -0,0 +1,13 @@
+package com.EmpresaActivos.springboot.EmpresaActivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaActivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresaActivosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EmpresaActivos/springboot/EmpresaActivos/EmpresaActivosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmpresaActivos.springboot.EmpresaActivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaActivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
